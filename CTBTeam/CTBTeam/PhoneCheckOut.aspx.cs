4bb7615499c4bf53ce8a31590a2db5289568d24e
alna172872 <alna172872@CTBGROUPPC1>
2017-04-10 14:22:29 -0400

    Hello adrian this is your mother speaking please pick up eggs love you bye sweetie

--- CTBTeam/CTBTeam/PhoneCheckOut.aspx.cs
+++ CTBTeam/CTBTeam/PhoneCheckOut.aspx.cs
@@ -19,21 +19,21 @@ namespace CTBTeam
         TextBox[] textBoxes;
         DropDownList dlist = new DropDownList();
         String[] list = new String[3];
-
+        DropDownList drpPhone = new DropDownList();
 
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+
 
             if (!IsPostBack)
             {
 
-           
-                                                     
+
+
             }
 
         }
-        
+
         protected void populateDataPhones()
         {
             try
@@ -47,12 +47,12 @@ namespace CTBTeam
                 OleDbDataReader reader = objCmdSelect.ExecuteReader();
                 while (reader.Read())
                 {
-                    dlist.Items.Add(new ListItem(reader.GetString(1)));;
+                    dlist.Items.Add(new ListItem(reader.GetString(1))); ;
                 }
 
                 objConn.Close();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 if (!System.IO.File.Exists(@"" + Server.MapPath("~/Debug/StackTrace.txt")))
                 {
@@ -63,8 +63,49 @@ namespace CTBTeam
                     file.WriteLine(Date.Today.ToString() + "--Populate Phones--" + e.ToString());
                     file.Close();
                 }
-            
+            }
         }
+
+
+        public void pop2()
+        {
+            //String str = dlist.SelectedItem.Text;
+            try
+            {
+                drpPhone.Items.Clear();
+                String connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                         "Data Source=" + Server.MapPath("~/CTBWebsiteData.accdb") + ";";
+                OleDbConnection objConn = new OleDbConnection(connectionString);
+                objConn.Open();
+
+                OleDbCommand objCmdSelect = new OleDbCommand("SELECT Model FROM PhoneCheckout WHERE OS=@str;", objConn);
+                objCmdSelect.Parameters.AddWithValue("@str", dlist.SelectedItem.Text);
+                
+                OleDbDataReader reader = objCmdSelect.ExecuteReader();
+                while (reader.Read())
+                {
+                    dlist.Items.Add(new ListItem(reader.GetString(1))); ;
+                }
+
+                objConn.Close();
+            }
+            catch (Exception e)
+            {
+                if (!System.IO.File.Exists(@"" + Server.MapPath("~/Debug/StackTrace.txt")))
+                {
+                    System.IO.File.Create(@"" + Server.MapPath("~/Debug/StackTrace.txt"));
+                }
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"" + Server.MapPath("~/Debug/StackTrace.txt")))
+                {
+                    file.WriteLine(Date.Today.ToString() + "--Populate Phones--" + e.ToString());
+                    file.Close();
+                }
+            }
+
+        }
+    }
+}
+
        
         
 
@@ -74,6 +115,3 @@ namespace CTBTeam
 
 
 
-
-    }
-}
