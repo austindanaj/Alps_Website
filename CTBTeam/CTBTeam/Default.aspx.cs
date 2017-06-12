2e0d2415604be46da044cd914e769e0cddb776db
anthony hewins <alna173017@gl.alps.com>
2017-06-12 12:56:35 -0400

    stash again

--- CTBTeam/CTBTeam/Default.aspx.cs
+++ CTBTeam/CTBTeam/Default.aspx.cs
@@ -3,28 +3,42 @@ using System.Data.SqlClient;
 using System.Data;
 using Date = System.DateTime;
 using System.Collections;
+using System.IO;
 
 namespace CTBTeam {
 	public partial class _Default : SuperPage {
 		protected void Page_Load(object sender, EventArgs e) {
-			
+
 		}
 
 		protected void View_More_onClick(object sender, EventArgs e) {
 			redirectSafely("Hours.aspx");
 		}
-		
+
 		protected void download(object sender, EventArgs e) {
 			SqlConnection objConn = openDBConnection();
 			objConn.Open();
 
-			
+			SqlDataReader reader = getReader("select ID, Dates from Dates order by ID desc", null, objConn);
+			reader.Read();
+			Session["Date_ID"] = reader.GetInt32(0);
+			Session["Date"] = reader.GetDateTime(1);
+			reader.Close();
 
-			DataTable tempDataTable = new DataTable();      //Data table to temporarily store the table before the gridview
+			DataTable allEmployees = getDataTable("select Alna_num, Name from Employees where Active=@value1", true, objConn);
+			object[] parameters = { true, false };
+			DataTable partTimeEmployees = getDataTable("select Alna_num, Name from Employees where Active=@value1 and Full_time=@value2", parameters, objConn);
+			DataTable projects = getDataTable("select ID, Name from Projects", null, objConn);
+			DataTable vehicles = getDataTable("select ID, Name from Vehicles where Active=@value1", true, objConn);
+			DataTable projectHours = getDataTable("select Alna_num, Proj_ID, Hours_worked from ProjectHours where Date_ID=@value1", Session["Date_ID"], objConn);
+			DataTable vehicleHours = getDataTable("select Alna_num, Vehicle_ID, Hours_worked from VehicleHours where Date_ID=@value1", Session["Date_ID"], objConn);
+			objConn.Close();
+
+			DataTable projectDataTable = new DataTable();      //Data table to temporarily store the table before the gridview
 			Hashtable h = new Hashtable();                  //Temp Hash table for projects and vehicles
 			Hashtable employeeHashTable = new Hashtable();  //Hash table for employees (there doesn't need to be 3 but it's simpler this way)
 
-			tempDataTable.Columns.Add("Name", typeof(string));
+			projectDataTable.Columns.Add("Name", typeof(string));
 
 			int i;    //Scrap int to use throughout the method: we have to reuse it a lot
 			string s;
@@ -33,14 +47,14 @@ namespace CTBTeam {
 				if (!(o is DataTable))
 					return;
 				DataTable d = (DataTable)o;
-				for (i = 0; i < smallestThreshold; i++) {       //Forall rows in projectData: 
+				for (i = 0; i < d.Rows.Count; i++) {       //Forall rows in projectData: 
 					s = d.Rows[i][1].ToString();                //Get the name of the project
-					h.Add(projectData.Rows[i][0], s);           //Add it to the hash table with the Proj_Id as the key
-					tempDataTable.Columns.Add(s, typeof(int));  //Add it as a column to the temporary datatable. The column accepts integer values because we're talking about hours worked
+					h.Add(projects.Rows[i][0], s);           //Add it to the hash table with the Proj_Id as the key
+					projectDataTable.Columns.Add(s, typeof(int));  //Add it as a column to the temporary datatable. The column accepts integer values because we're talking about hours worked
 				}
 			});
 
-			addColumns(projectData);
+			addColumns(projects);
 
 			i = 0;
 			DataRow temp;
@@ -48,9 +62,9 @@ namespace CTBTeam {
 			//Why do we need this? Because C# wont let you edit rows after they're added.
 			//So we have to put everything into an array first so we can add all the hours
 			//then we insert into the datatable.
-			DataRow[] tempTable = new DataRow[partTimeEmployeeData.Rows.Count];
-			foreach (DataRow d in partTimeEmployeeData.Rows) {
-				temp = tempDataTable.NewRow();  //Make a new row
+			DataRow[] tempTable = new DataRow[partTimeEmployees.Rows.Count];
+			foreach (DataRow d in partTimeEmployees.Rows) {
+				temp = projectDataTable.NewRow();  //Make a new row
 				temp["Name"] = d[1];            //The name of the employee is stored in [1]
 				employeeHashTable.Add(d[0], i); //Add the employee's ID to the hashtable to quickly get what row its in later
 				tempTable[i] = temp;            //Assign the new row to tempTable
@@ -72,39 +86,67 @@ namespace CTBTeam {
 				}
 			});
 
-			insertCells(projectHoursData);
+			insertCells(projectHours);
 
 			foreach (DataRow d in tempTable)
-				tempDataTable.Rows.Add(d);
-
-			dgvProject.DataSource = tempDataTable;
-			dgvProject.DataBind();
+				projectDataTable.Rows.Add(d);
 
 			//Done with projects, time to move onto the vehicles
 
-			tempDataTable = new DataTable();
+			DataTable vehicleDataTable = new DataTable();
 			h = new Hashtable();
 
-			tempDataTable.Columns.Add("Name", typeof(string));
-			smallestThreshold = requestedColNum >= vehiclesData.Rows.Count ? vehiclesData.Rows.Count : requestedColNum;
+			vehicleDataTable.Columns.Add("Name", typeof(string));
 
-			addColumns(vehiclesData);
+			addColumns(vehicles);
 
 			i = 0;
-			foreach (DataRow d in partTimeEmployeeData.Rows) {
-				temp = tempDataTable.NewRow();
+			foreach (DataRow d in partTimeEmployees.Rows) {
+				temp = vehicleDataTable.NewRow();
 				temp["Name"] = d[1];
 				tempTable[i] = temp;
 				i++;
 			}
 
-			insertCells(vehicleHoursData);
+			insertCells(vehicleHours);
 
 			foreach (DataRow d in tempTable)
-				tempDataTable.Rows.Add(d);
+				vehicleDataTable.Rows.Add(d);
 
-			dgvCars.DataSource = tempDataTable;
-			dgvCars.DataBind();
+			File.Create(@"" + Server.MapPath("~/Logs/DBLog.txt"));
+			StreamWriter file = new StreamWriter(@"" + Server.MapPath("~/Logs/DBLog.txt"));
+
+			addColumns = new Lambda(delegate (object o) {
+				DataTable tmp = (DataTable)o;
+				s = "Name,";
+				foreach (DataColumn d in tmp.Columns)
+					s += d.ToString() + ",";
+				file.Write(s);
+				file.WriteLine();
+			});
+
+			addColumns(projectDataTable);
+
+			Lambda insertRows = new Lambda(delegate (object o) {
+				DataTable tmp = (DataTable)o;
+				foreach (DataRow d in tmp.Rows) {
+					s = "";
+					foreach (object obj in d.ItemArray)
+						s += obj.ToString() + ",";
+					file.Write(s);
+					file.WriteLine();
+				}
+			});
+
+			addColumns(projectDataTable);
+			insertRows(projectDataTable);
+			file.WriteLine();
+			addColumns(vehicleDataTable);
+			insertRows(vehicleDataTable);
+
+			file.Close();
+
+
 		}
 	}
 }
