68f899c09585f60143e5f5fc13dcb705fc4214e1
Austin Danaj <afdanaj@oakland.edu>
2017-08-10 09:49:16 -0400

    a

---
+++ CTBTeam/CTBTeam/IssueList.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Issue List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="IssueList.aspx.cs" Inherits="CTBTeam.IssueList" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+	<style type="text/css">
+		body {
+			background: url('Gradient.jpg') no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+	</style>
+    <asp:TextBox ID="successOrFail" runat="server" Text="Success." Visible="false" ReadOnly="true" CssClass="feedback-textbox" />
+    <asp:Button ID="btnEmail" runat="server" Text="Send Email" OnClick="btnSendEmail_Click" />
+
+
+</asp:Content>
---
+++ CTBTeam/CTBTeam/IssueList.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Date = System.DateTime;
+using System.Data.OleDb;
+using System.Data;
+using System.Net.Mail;
+
+namespace CTBTeam {
+	public partial class IssueList : SuperPage {
+
+		protected void Page_Load(object sender, EventArgs e) {
+			if (!IsPostBack) {
+				
+			}
+		}
+
+	    protected void btnSendEmail_Click(object sender, EventArgs e)
+	    {
+	        try
+	        {
+	            MailMessage mail = new MailMessage();
+	            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+	            mail.From = new MailAddress("alnaandroidtest@gmail.com");
+	            mail.Subject = "--Please No Reply--";
+	            mail.Body = "This is a test email!";
+
+	            SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("alnaandroidtest", "alnatest");
+	            SmtpServer.EnableSsl = true;
+
+	            SmtpServer.Send(mail);
+	            successDialog(successOrFail);
+            }
+	        catch (Exception ex)
+	        {
+	            
+	        }
+	    }
+	}
+}
